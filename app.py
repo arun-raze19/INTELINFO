b07f04a07f6fcbcb544278650b97e230c1776e45
arun-raze19 <arunkumar1974pro@gmail.com>
2025-09-17 13:01:13 +0530

    Update app.py to ensure Flask runs on 0.0.0.0 for Docker compatibility

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def coming_soon():
-    return '<h1>Coming Soon</h1>'
+    return '<h1>Coming Soon  THE ARKA 2025</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
